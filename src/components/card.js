f57146eed5e2ee61f0b9ca80080cc2c926d0202a
ardanimo <ardanimoxd@gmail.com>
2025-01-30 11:55:43 +0300

    card animation

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,12 @@
+import {motion} from "framer-motion"
+
 export default function Card({name, description, keywords}) {
     return (
-    <div className="bg-zinc-500 opacity-100 text-white mx-auto mb-8 min-w-[300px] max-w-[800px] rounded-xl font-sans shadow-xl">
+    <motion.div 
+    whileInView={{ x: [-20, 10, -5, 0] }}
+    transition={{duration: 0.5, ease: "easeInOut"}}
+    className="bg-zinc-500 opacity-100 text-white mx-auto mb-8 min-w-[300px] max-w-[800px] rounded-xl font-sans shadow-xl"
+    >
         <div className="py-2 px-4 flex flex-col">
             <section className="min-h-[40px] flex items-center justify-center text-cyan-300 text-xl border-b-[0.5px] border-gray-400">{name}</section>
             <section className="text-left flex items-center justify-start min-h-[60px] text-lg border-b-[0.5px] border-gray-400 py-2">{description}</section>
@@ -8,6 +14,6 @@ export default function Card({name, description, keywords}) {
                 {keywords.map(keyword => (<p className="mx-2">{keyword}</p>))}
             </section>
         </div>
-    </div>
+    </motion.div>
     )
 }
