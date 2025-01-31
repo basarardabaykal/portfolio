5e8e301899c3f3fc64fee063d78a4da373172f4c
ardanimo <ardanimoxd@gmail.com>
2025-01-31 12:36:20 +0300

    link text wrap fix

--- src/components/card.js
+++ src/components/card.js
@@ -10,8 +10,8 @@ export default function Card({name, links, description, keywords}) {
         <div className="py-2 px-4 flex flex-col">
             <section className="min-h-[40px] flex flex-col items-center justify-center text-cyan-300 border-b-[0.5px] border-gray-400">
                 <div className="lg:text-xl text-base">{name}</div>
-                <div className="flex flex-col lg:text-base text-xs text-cyan-400">
-                    {links ? links.map(link => (<a href={link} target="_blank">{link}</a>)) : <p></p>}
+                <div className="flex flex-col lg:text-base text-xs text-cyan-400 overflow-hidden w-full">
+                    {links ? links.map(link => (<a href={link} target="_blank" className="break-words">{link}</a>)) : <p></p>}
                 </div>
                 </section>
             <section className="text-left flex items-center justify-start min-h-[60px] lg:text-lg text-sm border-b-[0.5px] border-gray-400 py-2">{description}</section>
